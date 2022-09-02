fa2e05d775c4f477e6dbf3fa41e60401e3c000df
Barbara Balen <barbarabalen@barbaras-air.localdomain>
2022-09-02 11:25:05 +0200

    add shopping item

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { useState } from 'react';
-import './App.css';
-
-const ApiUrl='https://pokeapi.co/api/v2/item/'
-
-const[shopInventory, setShopInventory]=useState([]);
-
-fetch(ApiUrl)
-.then((response)=>response.json())
-.then((data)=> setShopInventory(data.results)
-.catch((error)=> console.log(error))
-
- )
-
-
-
-
+import { useEffect } from "react";
+import { useState } from "react";
+import "./App.css";
+import ShoppingItem from "./components/ShoppingItem";
 
 function App() {
-  
-  
-  
+  const ApiUrl = "https://pokeapi.co/api/v2/item/";
+  const [shopInventory, setShopInventory] = useState([]);
+
+  useEffect(() => {
+    fetch(ApiUrl)
+      .then((response) => response.json())
+      .then((data) => setShopInventory(data.results));
+  }, []);
+
+  console.log(shopInventory);
+
   return (
     <div className="App">
       <h1>Poke Mart Online Shop</h1>
+      <ul>
+        {shopInventory.map((item) => {
+          return <ShoppingItem key={item.name} name={item.name} />;
+        })}
+      </ul>
     </div>
   );
 }
