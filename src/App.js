3b14d81277a11c16cca6d0af580262ea02fbc230
Dejan Jankovic <deki1984@hotmail.de>
2022-09-02 10:59:43 +0200

    add fetch initialise state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-
+import { useState } from 'react';
 import './App.css';
 
+const ApiUrl='https://pokeapi.co/api/v2/item/'
+
+const[shopInventory, setShopInventory]=useState([]);
+
+fetch(ApiUrl)
+.then((response)=>response.json())
+.then((data)=> setShopInventory(data.results)
+.catch((error)=> console.log(error))
+
+ )
+
+
+
+
+
 function App() {
+  
+  
+  
   return (
     <div className="App">
       <h1>Poke Mart Online Shop</h1>
